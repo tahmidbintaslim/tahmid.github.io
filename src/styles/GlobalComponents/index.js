d23c0aa5f856083b3ca907bbc01ac42f2b4fb6ee
Tahmid Bin Taslim Rafi <tahmidbintaslimrafi@gmail.com>
2024-02-18 22:22:14 +0000

    Style Change

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -56,16 +56,16 @@ export const SectionTitle = styled.h2`
 
 export const SectionText = styled.p`
   max-width: 800px;
-  font-size: 24px;
-  line-height: 40px;
+  font-size: 20px;
+  line-height: 30px;
   font-weight: 300;
   padding-bottom: 3.6rem;
-  color: rgba(255, 255, 255, 0.5);
+  color: rgba(255, 255, 255, 0.75);
 
   @media ${(props) => props.theme.breakpoints.md} {
     max-width: 670px;
-    font-size: 20px;
-    line-height: 32px;
+    font-size: 18px;
+    line-height: 28px;
     padding-bottom: 24px;
   }
 
