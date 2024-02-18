6d48351c9e5cf174135e1b89fc1858a5d9dd9d9e
Tahmid Bin Taslim Rafi <tahmidbintaslimrafi@gmail.com>
2024-02-18 22:06:31 +0000

    Added Content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,7 +9,7 @@ const Hero = (props) => (
     <LeftSection>
       <SectionTitle main center >
         Welcome to <br />
-        Tahmid's World 💻
+        Tahmid's World !
       </SectionTitle>
       <SectionText >
         I am an accomplished Full Stack Developer with a strong foundation in Data Science and Software Engineering, obtained from Stamford International University in Bangkok. My professional journey began in 2018 with freelance projects during my university studies, evolving into contract and full-time roles before graduation. Over the past four years, I have leveraged my expertise across diverse industries, building engaging and functional websites and web applications. This experience has honed my skills and solidified my commitment to excellence in the tech field.
