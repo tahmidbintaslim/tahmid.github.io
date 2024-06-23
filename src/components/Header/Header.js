48315139fb45cfd6d71b48c769bb146ab824dccb
Tahmid Bin Taslim Rafi <tahmidbintaslimrafi@gmail.com>
2024-06-23 13:12:47 +0000

    refactor: Remove unnecessary code and fix header logo display issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,14 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
-          <DiCssdeck size="3rem" /> <Span> <img src="https://avatars.githubusercontent.com/u/45480402?v=4" alt="Tahmid Bin Taslim Rafi" title="Tahmid Bin Taslim Rafi style={{ borderRadius: "50%", width: "30px", height: "30px" }} /> </Span>
-        </a>
+          <DiCssdeck size="3rem" /></a>
       </Link>
     </Div1>
     <Div2>
