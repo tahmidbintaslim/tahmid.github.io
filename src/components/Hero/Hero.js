82b571c027d80184bc120d28a24f63852da5cd0e
Tahmid Bin Taslim Rafi <tahmidbintaslimrafi@gmail.com>
2024-10-15 07:41:53 +0000

    Refactor Hero component to add id attribute to the container element

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <Section row nopadding>
+  <Section row nopadding id="hero">
     <LeftSection>
       <SectionTitle main center >
         Welcome to <br />
